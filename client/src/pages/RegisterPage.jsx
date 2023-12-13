8e6a9c4387358e6ab4de053e63de79b6fc60bae7
Tiffany <thuytliem@gmail.com>
2023-12-12 22:57:18 -0600

    Added post request for login page

--- client/src/pages/RegisterPage.jsx
+++ client/src/pages/RegisterPage.jsx
@@ -6,13 +6,18 @@ export default function RegisterPage() {
   const [name,setName] = useState('');
   const[email,setEmail] = useState('');
   const [password,setPassword] = useState('');
-  function registerUser(e){
+  async function registerUser(e){
     e.preventDefault();
-    axios.post('/register', {
+    try {
+      await axios.post('/register', {
       name,
       email,
       password,
     });
+    alert('Registration successful. Now you can log in');
+    } catch (error){
+      alert('Registration failed. Please try again later');
+    }
   }
   return (
     <div className="mt-4 grow flex items-center justify-around">
